4934b0394b307ceca8f3e00d74b7b95d33cd3f80
maks2001916 <mamonov.maksim.20003@gmail.com>
2024-10-14 19:31:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/e_book/Database.kt
@@ -0,0 +1,9 @@
+package com.example.e_book
+
+class Database {
+    var text: String =
+        "Беседа пьяного с трезвым чёртом" +
+        "Чёрт сконфузился и потупил глазки."
+                get() = text
+
+}
---
+++ app/src/main/java/com/example/e_book/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.e_book
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var headingTV: TextView
+    private lateinit var downloadBTN: Button
+    private lateinit var textTV: TextView
+    private lateinit var database: Database
+
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        headingTV = findViewById(R.id.headingTV)
+        downloadBTN = findViewById(R.id.downloadBTN)
+        textTV = findViewById(R.id.textTV)
+        database = Database()
+        downloadBTN.setOnClickListener {
+            val book = loadBook(database.text)
+            for (i in book ) textTV.text = "$i \n"
+        }
+    }
+
+    fun loadBook(text: String): List<String> {
+        return text.split(" ") }
+}
